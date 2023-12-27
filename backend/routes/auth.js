aaec30f823d6d3bf4721f7c5787c3dfdde3757aa
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2023-12-27 13:18:45 +0530

    Storing Data into Database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const User = require('../models/user')
 const router = express.Router();
 
-router.get('/', (req, res) =>{
-    obj = {
-        a : "thios",
-        number : 7
-    }
-    res.json(obj)
+router.post('/', (req, res) =>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
