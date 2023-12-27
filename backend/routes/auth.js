d6d6b27826ed0483d49b58d5467bc46e71def07b
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2023-12-27 19:08:40 +0530

    Added Data Validations

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const User = require('../models/user')
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.post('/', (req, res) =>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+// Create a user using POST "/api/auth", doesn't require authentication
+
+router.post('/',[
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be at least 5 charaters').isLength({ min: 5 }),
+
+] ,(req, res) =>{
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error : 'Please enter a valid email address', message : err.message})})
 })
 
 module.exports = router
